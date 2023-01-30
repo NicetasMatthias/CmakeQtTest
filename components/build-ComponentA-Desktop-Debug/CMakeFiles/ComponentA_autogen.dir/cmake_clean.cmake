60f0044d2f6920530e4251b7c35dc9d8ea4577e9
MatveevND <MatveevND@inteltech.ru>
2023-01-30 10:20:42 +0300

    buid директории добавлены в .gitignore

--- components/build-ComponentA-Desktop-Debug/CMakeFiles/ComponentA_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/ComponentA_autogen"
-  "ComponentA_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/ComponentA_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
